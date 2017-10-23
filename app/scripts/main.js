1e8d49bf2907d6360c7056e11475fea02439a02c
Chris <stac1221@gmail.com>
2017-10-22 20:40:46 -0500

    added makeCode function, js responsive position colorSelector and submit btn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,24 @@
+'use strict';
+
 $(document).ready(function() {
 
   let bgColor = '';
+  
+  console.log(makeCode());
+  
+  $('body').css('overflow-y', 'hidden');
+  let vpWidth = $(window).width();
+  console.log('vpWidth', vpWidth);
+  $('body').css('overflow-y', 'visible');
+  
+  let gameBoardWidth = $('#gameboard').outerWidth();  
+  
+  let posColorSelector = ((vpWidth - gameBoardWidth) / 2) - 120;  
+  $('#color-selector').css('left', posColorSelector); 
+
+  let posSubmitButton = ((vpWidth - gameBoardWidth) / 2) - 150;
+  console.log('posSubmitButton', posSubmitButton);
+  $('#submit').css('right', posSubmitButton);  
 
   $('#color-selector .cdBkr-spot').click(function() {   
     $('#color-selector .cdBkr-spot').css('border-width', '2px');
@@ -13,6 +31,36 @@ $(document).ready(function() {
 
   $('.row.active .cdBkr-spot').click(function() {      
     $(this).css('background-color', bgColor);
-  }); 
+    $(this).addClass('guess-made');
+  });
+  // if ('.row.active .cdBkr-spot-1') 
+
+  function makeCode() {
+    let codeArray = [];
+    for (let i = 0; i < 4; i++) {
+      codeArray.push(Math.floor(Math.random() * 6));
+    }
+    return codeArray;
+  } 
+  makeCode();
+
+  function updateContainer() {
+    $('body').css('overflow-y', 'hidden');
+    let vpWidth = $(window).width();
+    console.log('vpWidth', vpWidth);
+    $('body').css('overflow-y', 'visible');
+
+    let gameBoardWidth = $('#gameboard').outerWidth(); 
+
+    let posColorSelector = ((vpWidth - gameBoardWidth) / 2) - 120;    
+    $('#color-selector').css('left', posColorSelector);
+
+    let posSubmitButton = ((vpWidth - gameBoardWidth) / 2) - 150;    
+    $('#submit').css('right', posSubmitButton);
+  }
+
+  $(window).resize(function() {
+    updateContainer();
+  });  
 
 });
